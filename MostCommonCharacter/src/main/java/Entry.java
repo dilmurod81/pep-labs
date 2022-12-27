d7c378aa9db86895fe3ba4040d59fcc6d359b61e
Dilmurod Berdiev <berdievdilmurod@gmail.com>
2022-12-27 22:37:43 +0000

    Saved progress on MostCommonCharacter 2022-12-27 22:37:43 +0000

---
+++ MostCommonCharacter/src/main/java/Entry.java
@@ -0,0 +1,4 @@
+
+public class Entry<T1, T2> {
+
+}
--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +12,30 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        char [] eachChar = str.toCharArray();
+        for (char c : eachChar){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)+1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        Set<Entry<Character, Integer>> entrySet = map.entrySet();
+         
+        int maxCount = 0;
+         
+        char maxChar = 0;
+         
+        for (Entry<Character, Integer> entry : entrySet) 
+        {
+            if (entry.getValue() > maxCount)
+            {
+                maxCount = entry.getValue();
+                 
+                maxChar = entry.getKey();
+            }
+        }
+        return maxChar;
     }
 }
