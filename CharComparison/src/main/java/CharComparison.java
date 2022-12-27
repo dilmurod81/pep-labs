83530c214ef1223ce6d0cc23f803a9da96c9459d
Dilmurod Berdiev <berdievdilmurod@gmail.com>
2022-12-27 18:23:47 +0000

    Saved progress on CharComparison 2022-12-27 18:23:47 +0000

--- CharComparison/src/main/java/CharComparison.java
+++ CharComparison/src/main/java/CharComparison.java
@@ -1,3 +1,5 @@
+import org.eclipse.jetty.util.LeakDetector.LeakInfo;
+
 public class CharComparison {
     /**
      * Given an array of char, determine if the array A belongs to the left or to the right of char array B lexigraphically.
@@ -11,6 +13,17 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
+        for (int i = 0; i < a.length; i++){
+            for(int j = 0; j < b.length; j++){
+                if(a[i] > b[j]){
+                    return 1;
+                }else if (a[i] < b[j]){
+                    return -1;
+                }else if (a[i] == b[j]){
+                    return 0;
+                }
+            }
+        }
         return 0;
     }
 }
