55d0a9b251851ee256ea7010e5b9ae6103124baa
Dilmurod Berdiev <berdievdilmurod@gmail.com>
2022-12-28 03:57:54 +0000

    Saved progress on Pyramid 2022-12-28 03:57:54 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE post (id SERIAL PRIMARY KEY, post VARCHAR(255), user_fk INT, FOREIGN KEY (user_fk) REFERENCES user.user(id));
+CREATE TABLE post (id SERIAL PRIMARY KEY, post VARCHAR(255), user_fk INT FOREIGN KEY REFERENCES user(id));
