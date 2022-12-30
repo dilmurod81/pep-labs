370052ec6dbc07414a28533b951f90bb8c347459
Dilmurod Berdiev <berdievdilmurod@gmail.com>
2022-12-30 21:27:26 +0000

    Saved progress on Instantiation 2022-12-30 21:27:26 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,13 +27,25 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        System.out.print("size" + n + ":");
-        for(int i = 1; i <= n; ++i){
-            for(int j = 1; j <= i; ++j){
-                System.out.print("*");
+        int rows = n, i = 1, j = 1;
+
+        while (i <= rows) {
+            while (j <= i) {
+                return "*";
+                j++;
+
             }
-            System.out.print("\n");
+           return "\n";
+            i++;
+            j = 1;
         }
         return "";
+        // for(int i = 1; i <= n; ++i){
+        //     for(int j = 1; j <= i; ++j){
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
+        // return "";
     }
 }
